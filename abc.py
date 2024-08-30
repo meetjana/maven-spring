5cb5fc22aa8e8a39084b5d90b545aa6f1912ecea
meetjana <meetjana.au.hp@gmail.com>
2024-08-30 10:06:58 +0530

    code edited

--- abc.py
+++ abc.py
@@ -1,1 +1,13 @@
-print("Hello World")
+# Open the file in read mode
+file_path = 'version'
+try:
+    with open(file_path, 'r') as file:
+        # Read the content of the file
+        file_content = file.read()
+        # Print the content
+        print("File Content:\n", file_content)
+except FileNotFoundError:
+    print(f"File '{file_path}' not found.")
+except Exception as e:
+    print(f"An error occurred: {e}")
+
